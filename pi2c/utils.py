744987b8a7e11c0b9bbf314261fbf4a0a0ad8f61
cvoelcker (iliricon) <claas@voelcker.net>
2020-04-05 20:19:21 +0200

    Fixed conditioning

--- pi2c/utils.py
+++ pi2c/utils.py
@@ -270,13 +270,13 @@ class GMM(Distribution):
         for i in range(self.num_components):
             # correction for 1 dim u (linalg doesn't invert that)
             if len(var_mask) == 1:
-                sig_corr = self.sig[i][idx, idx] * 1/self.sig[i][idx, idx]
+                sig_corr = self.sig[i][var_mask, idx] * 1/self.sig[i][idx, idx]
             else:
-                sig_corr = self.sig[i][var_mask, idx].dot(self.sig[i][var_mask, idx]).dot(
-                    np.linalg.inv(self.sig[i][idx, idx]))
+                sig_corr = self.sig[i][var_mask, idx].dot(
+                        np.linalg.inv(self.sig[i][idx, idx]))
             gmm_var.mu[i] += sig_corr.dot(x - gmm_obs.mu[i])
             gmm_var.sig[i] -= sig_corr.dot(self.sig[i][idx, var_mask])
-
+        gmm_var.init_pdf()
         return gmm_var
 
     def conditional_mean(self, x, idx):
