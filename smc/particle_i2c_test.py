10a1052cbce099b8396ca3455151cc612326495f
cvoelcker (iliricon) <claas@voelcker.net>
2020-04-26 14:18:10 +0200

    Added plotting to test script

--- smc/particle_i2c_test.py
+++ smc/particle_i2c_test.py
@@ -55,6 +55,7 @@ if __name__ == "__main__":
     alpha = 1e-5
     particle_graph = ParticleI2cGraph(
         sys, cost, 100, num_p, num_p//10, np.array([5., 5.]), 0.1, np.array([0., 0., 0.]), 10000., alpha, 1, u_samples, num_runs)
+    plotter = ParticleI2cPlotter(particle_graph)
 
     costs_over_run = []
     alpha_over_run = []
@@ -68,6 +69,7 @@ if __name__ == "__main__":
         # alpha_over_run.append(alpha)
         # alpha = np.clip(particle_graph.run(alpha, False, 1), 0.66*alpha, 1.5*alpha)
         alpha = particle_graph.run(alpha, False, 1)
+        plotter.plot_all('Round ' + i)
         
         print('Updated graph {}, new alpha {}'.format(i, alpha))
 
