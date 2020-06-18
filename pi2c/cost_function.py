Sign error fixed

pi2c/cost_function.py
@@ -96,7 +96,7 @@ class Cost2Prob():
         samples = Gumbel(loc=0., scale=1.).sample((x.shape[0],n))
         log_gumbel = alpha * costs + samples
         _, choices = torch.max(log_gumbel, 0)
-        return choices, alpha*costs
+        return choices, costs
 
     def cost_jax(self, x):
         return self.c.cost_jax(x)

pi2c/particle_i2c.py
@@ -283,8 +283,11 @@ class ParticleI2cGraph():
             update_alpha = _iter == (max_iter-1)
             alpha, loss, converged = self._maximization(weights, particles, update_alpha=update_alpha)
             if update_alpha:
-                print(alpha)
-                self.alpha = alpha
+                #TODO: Find out why the sign error exists
+                print()
+                print()
+                print(self.alpha)
+                self.alpha = -alpha
             losses.append(loss.detach().numpy())
         np.savetxt('{}/losses_{}.npy'.format(log_dir, self.log_id), losses)
         for c in self.cells: